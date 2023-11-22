93f41a20dd910e9433ae7a2337983f72b7970f58
Raphael <mora.raphael@gmail.com>
2023-11-22 12:12:19 +0100

    first

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1 v-html="headerText"></h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: {
+    headerText: String,
+  },
+};
+</script>
+
+<style>
+@import "../assets/styles/header.css";
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <section>
+      <div class="product-image">
+        <img
+          class="meme"
+          :src="
+            image
+              ? require('../images/meme.jpg')
+              : require('../images/images.jpg')
+          "
+          alt="les images"
+          @mouseover="hoverLink"
+          @mouseout="resetImage"
+        />
+      </div>
+      <AppArticle />
+    </section>
+  </main>
+</template>
+
+<script>
+import AppArticle from "./AppArticle.vue";
+
+export default {
+  components: {
+    AppArticle,
+  },
+  data() {
+    return {
+      image: false,
+    };
+  },
+  methods: {
+    hoverLink() {
+      this.image = true;
+    },
+    resetImage() {
+      this.image = false;
+    },
+  },
+};
+</script>
+
+<style>
+@import "../assets/styles/main.css";
+</style>
